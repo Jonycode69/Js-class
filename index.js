cf5107ed81b1503e401c951324ca15b4c5281d35
Prince Leo <ohagamichael@gmail.com>
2025-05-16 19:05:26 +0100

    added assignment 2 solution

--- index.js
+++ index.js
@@ -47,3 +47,56 @@ sort.addEventListener("change", (e) => {
     sortedMoviesContainer.innerHTML = moviesMapped;
   }
 });
+
+
+
+// Assignment 2
+
+// 1. Array of student objects
+const students = [
+  { name: "Alice", score: 85 }, //scored A
+  { name: "Bob", score: 58 }, //scored D
+  { name: "Charlie", score: 92 }, //scored A
+  { name: "Diana", score: 74 }, //scored C
+  { name: "Chris", score: 95, age: 20, country: "USA" }, //scored A
+];
+
+// 2. Function to assign grades
+function getGrade(score) {
+  if (score >= 90) return "A";
+  else if (score >= 80) return "B";
+  else if (score >= 70) return "C";
+  else if (score >= 60) return "D";
+  else return "F";
+}
+
+// 3. Process each student
+let totalScore = 0;
+
+for (let process = 0; process < students.length; process++) { // Loop through the array
+  const { name, score, age, country } = students[process]; // Destructure the object
+
+  // or without destructuring
+//   const name = students[i].name;
+//   const score = students[i].score;
+//   const age = students[i].age;
+//   const country = students[i].country;
+
+  const grade = getGrade(score); // Call the getGrade function
+  totalScore += score;
+
+//   console.log(`${name} scored ${score} and received a grade of ${grade}.`);
+  //display all students scores and received grades on the UI using innerHTML
+  const studentList = document.getElementById("StudentList");
+  studentList.innerHTML += `<li>${name} scored ${score} and received a grade of ${grade}.</li>`;
+
+
+  // 4. Check scholarship eligibility
+  if (score > 90 && name.startsWith("C")) {
+    console.log(`${name} is eligible for a scholarship!`);
+  }
+}
+
+// 5. Calculate average score
+const averageScore = totalScore / students.length;
+console.log(`\nClass average score: ${averageScore.toFixed(2)}`);
